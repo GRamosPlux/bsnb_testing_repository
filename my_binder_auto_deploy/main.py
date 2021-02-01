ce52ad922db6d2704f2209a2c48dac32bfec0cea
G. Ramos <gramos@plux.info>
2021-02-01 20:05:45 +0000

    First commit

---
+++ my_binder_auto_deploy/main.py
@@ -0,0 +1,44 @@
+# ======================================================================================================================
+# ============================================== Import Packages =======================================================
+# ======================================================================================================================
+import os
+import git
+from time import *
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< REQUIREMENTS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# ---> Git should be installed and configured in the computer where the script will be executed.
+
+# ======================================================================================================================
+# ============================================= Repository Info ========================================================
+# ======================================================================================================================
+root_path = ".." + os.sep
+
+# ======================================================================================================================
+# ====================================== Creating a Branch per Category ================================================
+# ======================================================================================================================
+# >>> Identification of the path for the local version of the repository
+#     (in the parent directory of the current project).
+curr_dir = os.path.dirname(os.path.realpath(""))
+repo = git.Repo(curr_dir)
+
+# >>> Creating the new branch reference.
+new_branch = 'test_branch'
+current = repo.create_head(new_branch)
+
+# >>> Checkout to the new branch.
+current.checkout()
+
+#creating file
+dtime = strftime('%d-%m-%Y %H:%M:%S', localtime())
+with open(curr_dir + os.sep + 'lastCommit' + '.txt', 'w') as f:
+    f.write(str(dtime))
+print('file created---------------------')
+
+# >>> Push data to the remote version of the repository.
+if repo.index.diff(None) or repo.untracked_files:
+    repo.git.add(A=True)
+    repo.git.commit(m='msg')
+    repo.git.push('--set-upstream', 'origin', current)
+    print('git push')
+else:
+    print('no changes')
